91a665657b9ecbaad26c42c868db6087dcc77eb3
sb7297 <sbaireddy7297@gmail.com>
2021-08-20 15:53:47 -0400

    test BubblePage

--- src/components/BubblePage.test.js
+++ src/components/BubblePage.test.js
@@ -2,12 +2,56 @@ import React from 'react';
 import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
+import fetchColorService from '../services/fetchColorService';
 import BubblePage from './BubblePage';
 
+jest.mock('../services/fetchColorService');
+
 test("Renders without errors", ()=> {
-    
+    fetchColorService.mockResolvedValueOnce({
+        data: [
+          {
+            color: "aliceblue",
+            code: {
+              hex: "#f0f8ff",
+            },
+            id: 1,
+          },
+          {
+            color: "limegreen",
+            code: {
+              hex: "#99ddbc",
+            },
+            id: 2,
+          },
+        ]
+    });
+    render(<BubblePage />);
 });
 
 test("Renders appropriate number of colors passed in through mock", async ()=> {
     //Keep in mind that our service is called on mount for this component.
+    fetchColorService.mockResolvedValueOnce({
+        data: [
+          {
+            color: "aliceblue",
+            code: {
+              hex: "#f0f8ff",
+            },
+            id: 1,
+          },
+          {
+            color: "limegreen",
+            code: {
+              hex: "#99ddbc",
+            },
+            id: 2,
+          },
+        ]
+    });
+
+    render(<BubblePage />);
+
+    const colors = await screen.findAllByTestId("color");
+    expect(colors).toHaveLength(2);
 });
